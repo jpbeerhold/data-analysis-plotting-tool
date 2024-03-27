20590f9ca4e0ea7a47674e81fd6580d7382155ff
jpbeerhold <148579658+jpbeerhold@users.noreply.github.com>
2024-03-27 17:47:39 +0100

    build

--- data_analysis_plotting_tools/Bool.py
+++ data_analysis_plotting_tools/Bool.py
@@ -1,4 +1,18 @@
-class Bool:
+class Bool():
+
+    def __init__(self, data_url, target_path, data_file) -> None:
+        """Download, preprocess, and plot politeness data from Bodo Winter
+
+        Parameters
+        ----------
+        data_url : str 
+            Url from where to download the data set.
+        target_path : str
+            local working directory where data should be stored.
+        data_file : str
+            name of file to which data should be saved.
+
+        """
 
     def iboo(self, name):
         """I boo around.
